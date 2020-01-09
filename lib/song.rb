b8bea417d32c765743f8d9e8042d6181e764a8ee
Joanna E K <kalemajoanna@gmail.com>
2020-01-09 23:13:18 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -19,9 +19,9 @@ include Paramable
     @@songs << self
   end
 
-  # def self.find_by_name(name)
-  #   @@songs.detect{|a| a.name == name}
-  # end
+  def self.find_by_name(name)
+    @@songs.detect{|a| a.name == name}
+  end
 
   def self.all
     @@songs
